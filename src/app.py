30a97bf39b5b1f5a29f22d850ece33864871b8a1
Drixner <drixnerrafael1@gmail.com>
2023-02-19 18:16:08 -0500

    add: conecion batabases

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 import os
 import database as db
 
@@ -13,13 +13,58 @@ app = Flask(__name__, template_folder=template_dir)
 
 
 # rutas de la aplicacion
-
 @app.route('/')
 def home():
-    return render_template('index.html')
+    cursor = db.database.cursor()
+    cursor.execute('SELECT * FROM users')
+    myresult = cursor.fetchall()
+    #convertir a diccionario
+    insertObject = []
+    columnName = [column[0] for column in cursor.description]
+    for record in myresult:
+        insertObject.append(dict(zip(columnName, record)))
+    cursor.close()
+    return render_template('index.html', data=insertObject)
+
+#ruta para guardar usuarios en la base de datos
+@app.route('/user', methods=['POST'])
+def addUser():
+    username = request.form['username']
+    name = request.form['name']
+    password = request.form['password']
 
 
+    if username and name and password:
+        cursor = db.database.cursor()
+        cursor.execute('INSERT INTO users (username, name, password) VALUES (%s, %s, %s)', (username, name, password))
+        db.database.commit()
+        cursor.close()
+    return redirect(url_for('home'))
 
+
+#ruta para eliminar usuarios de la base de datos
+
+@app.route('/delete/<string:id>')
+def delete(id):
+    cursor = db.database.cursor()
+    cursor.execute('DELETE FROM users WHERE id = {0}'.format(id))
+    db.database.commit()
+    cursor.close()
+    return redirect(url_for('home'))
+
+#ruta para actualizar usuarios de la base de datos
+@app.route('/edit/<string:id>', methods=['POST'])
+def edit(id):
+    username = request.form['username']
+    name = request.form['name']
+    password = request.form['password']
+
+    if username and name and password:
+        cursor = db.database.cursor()
+        cursor.execute('UPDATE users SET username = %s, name = %s, password = %s WHERE id = %s', (username, name, password, id))
+        db.database.commit()
+        cursor.close()
+    return redirect(url_for('home'))
 
 
 if __name__ == '__main__':
