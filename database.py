4482628275a6b1340e65eff6471e2a6c53f3c364
Drixner <drixnerrafael1@gmail.com>
2023-02-19 08:24:45 -0500

    feat: file base for init

---
+++ database.py
@@ -0,0 +1,9 @@
+import mysql.connector
+
+
+database = mysql.connector.connect(
+        host="localhost",
+        user="drix",
+        passwd="drixner123",
+        database="dbcrudbasic"
+        )
